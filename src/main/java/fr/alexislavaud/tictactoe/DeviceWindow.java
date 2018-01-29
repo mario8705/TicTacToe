89eff71f27a220f288cd50a5fcf7c489b2718454
Alexis Lavaud <alexislavaud191@gmail.com>
2018-01-29 16:28:00 +0100

    MacOS retina screen support

--- src/main/java/fr/alexislavaud/tictactoe/DeviceWindow.java
+++ src/main/java/fr/alexislavaud/tictactoe/DeviceWindow.java
@@ -11,6 +11,7 @@ public final class DeviceWindow
 {
     private long windowHandle;
     private int width, height;
+    private int fbWidth, fbHeight;
 
     /**
      * Attempt to create an OpenGL rendering context
@@ -41,6 +42,15 @@ public final class DeviceWindow
 
         GLFW.glfwMakeContextCurrent(windowHandle);
         GLFW.glfwSwapInterval(1); // Enable v-sync
+
+        {
+            final int[] fbWidthArray = new int[1];
+            final int[] fbHeightArray = new int[1];
+            GLFW.glfwGetFramebufferSize(this.windowHandle, fbWidthArray, fbHeightArray);
+
+            this.fbWidth = fbWidthArray[0];
+            this.fbHeight = fbHeightArray[0];
+        }
 
         GL.createCapabilities();
     }
@@ -100,6 +110,22 @@ public final class DeviceWindow
     public int getHeight()
     {
         return height;
+    }
+
+    /**
+     * Returns the width of the framebuffer
+     * @return The width of the framebuffer
+     */
+    public int getFramebufferWidth() {
+        return fbWidth;
+    }
+
+    /**
+     * Returns the height of the framebuffer
+     * @return The height of the framebuffer
+     */
+    public int getFramebufferHeight() {
+        return fbHeight;
     }
 
     /**
--- src/main/java/fr/alexislavaud/tictactoe/main/TicTacToe.java
+++ src/main/java/fr/alexislavaud/tictactoe/main/TicTacToe.java
@@ -110,7 +110,7 @@ public final class TicTacToe
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 
-        GL11.glViewport(0, 0, deviceWindow.getWidth(), deviceWindow.getHeight());
+        GL11.glViewport(0, 0, deviceWindow.getFramebufferWidth(), deviceWindow.getFramebufferHeight());
 
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
